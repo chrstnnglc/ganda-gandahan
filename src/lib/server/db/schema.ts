aa285abdb3a0629b7775aa1dc6c8942676f42fbd
chrstnnglc <catfelizardo@gmail.com>
2026-01-05 00:15:27 +0900

    feat: setup db

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,8 +1,27 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { generateRandomId } from '../utils';
 
-export const user = sqliteTable('user', {
+const timestamp = {
+	createdAt: integer('created_at', { mode: 'timestamp' })
+		.notNull()
+		.$defaultFn(() => new Date()),
+	updatedAt: integer('updated_at', { mode: 'timestamp' })
+		.notNull()
+		.$defaultFn(() => new Date())
+};
+
+const books = sqliteTable('books', {
+	...timestamp,
 	id: text('id')
 		.primaryKey()
-		.$defaultFn(() => crypto.randomUUID()),
-	age: integer('age')
+		.notNull()
+		.$defaultFn(() => generateRandomId()),
+	publicationDate: integer('publication_date', { mode: 'timestamp' }).notNull(),
+	title: text('title').notNull(),
+	author: text('author').notNull(),
+	isbn: integer('isbn').unique()
 });
+
+type InsertBookParams = typeof books.$inferInsert;
+
+export { books, type InsertBookParams };
