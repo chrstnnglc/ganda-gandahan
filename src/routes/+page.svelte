1261191a5928ee09ef736d15685f5b0697631681
chrstnnglc <catfelizardo@gmail.com>
2026-01-07 13:37:14 +0900

    feat: import page story to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import Button from "../stories/Button.svelte";
 	import Card from "../stories/Card.svelte";
-
+	import Page from "../stories/Page.svelte";
 </script>
 
-<Card cosmetic={}></Card>
-<Button label="Add Cosmetic" primary={true}></Button>
+<!-- <Card cosmetic={}></Card>
+<Button label="Add Cosmetic" primary={true}></Button> -->
+<Page>
+    
+</Page>
