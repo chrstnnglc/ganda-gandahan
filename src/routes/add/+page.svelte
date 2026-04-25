c28766dfafcd459bb15340bb4fb8286e8e6b0be4
chrstnnglc <catfelizardo@gmail.com>
2026-04-25 11:45:46 +0900

    feat: added routing for adding products

---
+++ src/routes/add/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import ProductForm from '../../stories/ProductForm.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h1>Add a product</h1>
+
+<ProductForm brands={data.brands} productTypes={data.productTypes} />
